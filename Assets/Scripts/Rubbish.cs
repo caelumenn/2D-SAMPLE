f776cd6ec328fbd27b996eddf508b60def16e01e
Shaw Ying <destinydy1213@gmail.com>
2020-05-20 17:02:11 +0100

    Add pathfinding

--- Assets/Scripts/Rubbish.cs
+++ Assets/Scripts/Rubbish.cs
@@ -4,21 +4,65 @@ using UnityEngine;
 
 public class Rubbish : MonoBehaviour
 {
-    public float speed = 0.1f;
-    // Start is called before the first frame update
+    private float startPosX;
+    private float startPosY;
+    private bool isBeingHeld = false;
+    public Vector2 speed = new Vector2(0, 0);
+    public Vector2 acceleration = new Vector2(0, 0);
+    
+
+    void Start()
+    {
+        speed.Set(0, 0);
+        
+    }
+
+
     void OnTriggerEnter2D(Collider2D other) 
     {
         Debug.Log("collider");
-        speed = 0.0f;
+        
         GetComponent<BoxCollider2D>().isTrigger = false;
     }
-    // Update is called once per frame
+ 
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x += speed * Time.deltaTime;
-        transform.position = position;
+        speed += acceleration * Time.deltaTime;
+
+        if (isBeingHeld) 
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, 0);
+
+        }
+        else
+        {
+            Vector2 position = transform.position;
+            position += speed * Time.deltaTime;
+            transform.position = position;
+        }
     }
 
-    
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            isBeingHeld = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isBeingHeld = false;
+    }
+
 }
